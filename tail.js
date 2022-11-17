40154c206bd40ad5a1ce93ac0f7b1b333cdba5a6
Irha Ali <irhaali_97@hotmail.ca>
2022-11-17 13:42:33 -0500

    My own version of .tail function

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+//This looks like a brute force method
+let actual = tail([5,6,7]);
+
+assertEqual(actual.length, 2);
+assertEqual(actual[0], 5);
+assertEqual(actual[1], 6);
+//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+//assertEqual(tail([5]), []);
+//assertEqual(tail([]), []);
