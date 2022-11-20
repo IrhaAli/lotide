39a63fe6a2240d83c4a30e33184f2c2683dbf7dc
Irha Ali <irhaali_97@hotmail.ca>
2022-11-20 17:46:02 -0500

    Final edits for week 1 work of this project

--- tail.js
+++ tail.js
@@ -13,8 +13,8 @@ const tail = function(array) {
 let actual = tail([5,6,7]);
 
 assertEqual(actual.length, 2);
-assertEqual(actual[0], 5);
-assertEqual(actual[1], 6);
-//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-//assertEqual(tail([5]), []);
-//assertEqual(tail([]), []);
+assertEqual(actual[0], 6);
+assertEqual(actual[1], 7);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+assertEqual(tail([5]), []);
+assertEqual(tail([]), []);
